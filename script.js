931c9486d6084a7ed6b688f8b213e349eac8bc51
MatiasCardoso000 <cardosomatias.000@gmail.com>
2024-10-21 21:46:05 -0300

    cambio de estilo a todos los elementos.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const todosDisplay = document.querySelector(".todos-container");
 const input = document.getElementById("add-todo");
 const form = document.querySelector(".add-todo-form");
+const todoItem = document.querySelector("todo");
 
 let todo = "";
 let todos = [];
@@ -16,50 +17,49 @@ form.addEventListener("submit", (e) => {
 input.addEventListener("input", ({ target }) => (todo = target.value));
 
 const showTodoOnDisplay = () => {
-  const { div, deleteBtn, inputCheckBox, img, todoItem, label, checkDiv } =
-    createElements();
   todos.forEach((todo) => {
     todoItem.textContent = `${
       todo.title.slice(0, 1).toUpperCase() + todo.title.slice(1)
     }`;
-    div.append(todoItem, inputCheckBox, label, deleteBtn);
     img.setAttribute("src", "./icon/compartimiento.png");
-    deleteBtn.append(img);
-    deleteBtn.addEventListener("click", deleteTodo(todo.id));
+
+    div.append(todoItem);
+
     todosDisplay.append(div);
   });
 };
 
-const createElements = () => {
-  const div = document.createElement("div");
-  const deleteBtn = document.createElement("button");
-  const label = document.createElement("label");
-  const inputCheckBox = document.createElement("input");
-  const checkDiv = document.createElement("div");
-  const img = document.createElement("img");
-  const todoItem = document.createElement("p");
-  label.appendChild(checkDiv);
-  label.className = "checkbox-container";
-  label.htmlFor = "check-todo";
-  inputCheckBox.id = "check-todo";
-  deleteBtn.className = "delete-btn";
-  todoItem.className = "todo";
-  div.className = "todo-item";
-  inputCheckBox.type = "checkbox";
+// const createElements = () => {
+//   const div = document.createElement("div");
+//   const deleteBtn = document.createElement("button");
+//   const label = document.createElement("label");
+//   const inputCheckBox = document.createElement("input");
+//   const checkDiv = document.createElement("div");
+//   const img = document.createElement("img");
+//   const todoItem = document.createElement("p");
 
-  return {
-    div,
-    deleteBtn,
-    img,
-    todoItem,
-    label,
-    inputCheckBox,
-  };
-};
+//   label.className = "checkbox-container";
+//   label.htmlFor = "check-todo";
+//   inputCheckBox.id = "check-todo";
+//   deleteBtn.className = "delete-btn";
+//   todoItem.className = "todo";
+//   div.className = "todo-item";
+//   inputCheckBox.type = "checkbox";
+
+//   return {
+//     div,
+//     deleteBtn,
+//     img,
+//     todoItem,
+//     label,
+//     inputCheckBox,
+//     checkDiv,
+//   };
+// };
 
 const checkIfInputIsEmpty = () => {
   if (input.validity.valueMissing || todo.trim() === "") {
-    input.setCustomValidity("Tiene que ingresar un todo");
+    input.setCustomValidity("Tiene que ingresar una tarea");
     todos = [];
   } else {
     todos.push({ id: Date.now(), title: todo });
